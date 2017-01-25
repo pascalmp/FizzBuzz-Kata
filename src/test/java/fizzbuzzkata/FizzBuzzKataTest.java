f9d2a26b94c37ce8bdcbd1919b919aa66bcfc713
Pascal Mpawenayo <pm@gmail.com>
2017-01-24 19:47:23 -0500

    Refactoring test class

--- src/test/java/fizzbuzzkata/FizzBuzzKataTest.java
+++ src/test/java/fizzbuzzkata/FizzBuzzKataTest.java
@@ -2,22 +2,31 @@ package fizzbuzzkata;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzKataTest {
+	
+	FizzBuzz testObject;
+	
+	@Before
+	public void setUp(){
+	  
+		testObject = new FizzBuzz();
+	}
 	
 	//Test if testObjectFizzBuzz returns the same number it is passed
 	
 	@Test
 	public void testObjectFizzBuzzShouldReturnANumberItIsPassed(){
-		FizzBuzz testObject = new FizzBuzz();
+		
 		assertEquals("1", testObject.fizzBuzz(1));
 		
 	}
 	//Test to verify if testObjectFizzBuzz returns the same number it is passed every time,
 	@Test
 	public void testObjectFizzBuzzShouldReturn2whenPassedTwo(){
-		FizzBuzz testObject = new FizzBuzz();
+		
 		assertEquals("2", testObject.fizzBuzz(2));
 	}
 
